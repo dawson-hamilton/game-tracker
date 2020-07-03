45cba50ac1c2ecc14ff0b14a217aca3de5b30d62
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-07-02 20:32:49 -0600

    trying to get the client folder to show up in github

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from "./components/Signup";
+
+function App() {
+  return (
+    <div>
+      <Signup />
+      <h1>this is a test</h1>
+    </div>
+  );
+}
+
+export default App;
